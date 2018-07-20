dee1734654d72261aef870896e242e5c6a54dfca
erdogan <aldemir.erdogan@gmail.com>
2018-07-20 21:02:18 +0300

    additional comments

--- nonZeroIndex.m
+++ nonZeroIndex.m
@@ -1,10 +1,10 @@
 function [r,c] = nonZeroIndex(image)
 % this file indicate first nonzero pixels locations
-             r=0; c=0;
+             r=0; c=0;  % initialize variables
             [Row, Col]= size(image);
 
-            for index1 = 1:Row
-                for index2 = 1:Col
+            for index1 = 1:Row  % scan row
+                for index2 = 1:Col  % scan colums
                     if(image(index2,index1)==1)
                         r=index1; 
                         c=index2;
