6be72207e729a6809868e3d8a309029149eda02c
erdogan <aldemir.erdogan@gmail.com>
2018-08-12 20:35:12 +0300

    new approach imroved

--- VCC/vertexChainCodeV_3.m
+++ VCC/vertexChainCodeV_3.m
@@ -40,18 +40,27 @@ end
 secondDiff = 2;
 vertexVector = [];
 patternDictionary = pattern_dictionary();
+indexFlag = 1;
+
 for index = 1: size(contour,1)
     fieldValue = vals(contour(index,1),contour(index,2));
     
     directionFlag = zeros(2,2);
-    C_contourIndex = contour(index:index+secondDiff,:);
+    
+    C_contourIndex = contour(indexFlag:indexFlag+secondDiff,:);
     for indexDirectionFlag=1:secondDiff
         directionFlag(indexDirectionFlag,:) = [(C_contourIndex(2,indexDirectionFlag) - C_contourIndex(1,indexDirectionFlag))...
                                                (C_contourIndex(3,indexDirectionFlag) - C_contourIndex(2,indexDirectionFlag))];          
     end
-        
+
+    if (mod(index,3)== 0)
+        indexFlag = indexFlag + 1 ;
+    end
+    
     if (sum(sum(directionFlag - patternDictionary(1).direction(:,:,1)))== 0)
         vertexVector = cat(2,vertexVector,fieldValue.kd);
+        
+        % donemecin ilkinde 2'li sonrakilerinde tekerli olacak...
     elseif (sum(sum(directionFlag - patternDictionary(1).direction(:,:,2)))== 0)
         vertexVector = cat(2,vertexVector,fieldValue.kd);
     
