0040c2c642b1e9ac320fa664b0c0fe46d67515a2
Erdogan Aldemir <aldemir.erdogan@gmail.com>
2018-08-13 11:41:56 +0300

    new truncated approch

--- VCC/vertexChainCodeV_4.m
+++ VCC/vertexChainCodeV_4.m
@@ -42,57 +42,55 @@ vertexVector = [];
 patternDictionary = pattern_dictionary();
 indexFlag = 1;
 
-for index = 1: size(contour,1)
+for index = 1:size(contour,1)
        
     directionFlag = zeros(2,2);
     
-    C_contourIndex = contour(indexFlag:indexFlag+secondDiff,:);
+    C_contourIndex = contour(index:index+secondDiff,:);
     for indexDirectionFlag=1:secondDiff
         directionFlag(indexDirectionFlag,:) = [(C_contourIndex(2,indexDirectionFlag) - C_contourIndex(1,indexDirectionFlag))...
                                                (C_contourIndex(3,indexDirectionFlag) - C_contourIndex(2,indexDirectionFlag))];          
     end
-    
-   for flagSwapping = 1:size(C_contourIndex,1)
-            fieldValue = vals(C_contourIndex(flagSwapping,1),C_contourIndex(flagSwapping,2));
-       
-            if (sum(sum(directionFlag - patternDictionary(1).direction(:,:,1)))== 0)
-                vertexVector = cat(2,vertexVector,fieldValue.kd);
-                % donemecin ilkinde 2'li sonrakilerinde tekerli olacak...
-            elseif (sum(sum(directionFlag - patternDictionary(1).direction(:,:,2)))== 0)
-                vertexVector = cat(2,vertexVector,fieldValue.kd);
 
-            elseif (sum(sum(directionFlag - patternDictionary(2).direction(:,:,1)))== 0)
-                if (localCounter == 1)
-                vertexVector = cat(2,vertexVector,fieldValue.kd);
-                end
-                localCounter = 0;
-                vertexVector = cat(2,vertexVector,fieldValue.gd);
-            elseif (sum(sum(directionFlag - patternDictionary(2).direction(:,:,2)))== 0)
-                vertexVector = cat(2,vertexVector,fieldValue.kd);
+    fieldValue = vals(C_contourIndex(index,1),C_contourIndex(index,2));
 
-            elseif (sum(sum(directionFlag - patternDictionary(3).direction(:,:,2)))== 0)
-                vertexVector = cat(2,vertexVector,fieldValue.kd);
-            elseif (sum(sum(directionFlag - patternDictionary(3).direction(:,:,1)))== 0)
-                vertexVector = cat(2,vertexVector,fieldValue.kd);
+    if (sum(sum(directionFlag - patternDictionary(1).direction(:,:,1)))== 0)
+        vertexVector = cat(2,vertexVector,fieldValue.kd);
+    elseif (sum(sum(directionFlag - patternDictionary(1).direction(:,:,2)))== 0)
+        vertexVector = cat(2,vertexVector,fieldValue.kd);
 
-            elseif (sum(sum(directionFlag - patternDictionary(4).direction(:,:,2)))== 0)
-                vertexVector = cat(2,vertexVector,fieldValue.kd);
-            elseif (sum(sum(directionFlag - patternDictionary(4).direction(:,:,1)))== 0)
-                vertexVector = cat(2,vertexVector,fieldValue.kd);
+    elseif (sum(sum(directionFlag - patternDictionary(2).direction(:,:,1)))== 0)
+        if (localCounter == 1)
+        vertexVector = cat(2,vertexVector,fieldValue.kd);
+        end
+        localCounter = 0;
+        vertexVector = cat(2,vertexVector,fieldValue.gd);
+    elseif (sum(sum(directionFlag - patternDictionary(2).direction(:,:,2)))== 0)
+        vertexVector = cat(2,vertexVector,fieldValue.kd);
 
-            elseif (sum(sum(directionFlag - patternDictionary(5).direction(:,:,2)))== 0)
-               if (localCounter == 1)
-                vertexVector = cat(2,vertexVector,fieldValue.gd);
-                end
-                localCounter = 0;
-                vertexVector = cat(2,vertexVector,fieldValue.gb);
-            elseif (sum(sum(directionFlag - patternDictionary(5).direction(:,:,1)))== 0)
-                vertexVector = cat(2,vertexVector,fieldValue.kd);
+    elseif (sum(sum(directionFlag - patternDictionary(3).direction(:,:,2)))== 0)
+        vertexVector = cat(2,vertexVector,fieldValue.kd);
+    elseif (sum(sum(directionFlag - patternDictionary(3).direction(:,:,1)))== 0)
+        vertexVector = cat(2,vertexVector,fieldValue.kd);
 
-            elseif (sum(sum(directionFlag - patternDictionary(6).direction(:,:,2)))== 0)
-                vertexVector = cat(2,vertexVector,fieldValue.kd);
-            elseif (sum(sum(directionFlag - patternDictionary(6).direction(:,:,1)))== 0)
-                vertexVector = cat(2,vertexVector,fieldValue.kd);
-            end
-   end    
+    elseif (sum(sum(directionFlag - patternDictionary(4).direction(:,:,2)))== 0)
+        vertexVector = cat(2,vertexVector,fieldValue.kd);
+    elseif (sum(sum(directionFlag - patternDictionary(4).direction(:,:,1)))== 0)
+        vertexVector = cat(2,vertexVector,fieldValue.kd);
+
+    elseif (sum(sum(directionFlag - patternDictionary(5).direction(:,:,2)))== 0)
+       if (localCounter == 1)
+        vertexVector = cat(2,vertexVector,fieldValue.gd);
+        end
+        localCounter = 0;
+        vertexVector = cat(2,vertexVector,fieldValue.gb);
+    elseif (sum(sum(directionFlag - patternDictionary(5).direction(:,:,1)))== 0)
+        vertexVector = cat(2,vertexVector,fieldValue.kd);
+
+    elseif (sum(sum(directionFlag - patternDictionary(6).direction(:,:,2)))== 0)
+        vertexVector = cat(2,vertexVector,fieldValue.kd);
+    elseif (sum(sum(directionFlag - patternDictionary(6).direction(:,:,1)))== 0)
+        vertexVector = cat(2,vertexVector,fieldValue.kd);
+    end
+  
 end
